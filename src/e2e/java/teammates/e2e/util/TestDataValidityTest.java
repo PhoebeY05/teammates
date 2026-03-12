43f0572aecff09c890571c967757f7d11d63ed73
Phoebe Yap <115935747+PhoebeY05@users.noreply.github.com>
2026-03-13 00:34:48 +0800

    Add handling for different naming

--- src/e2e/java/teammates/e2e/util/TestDataValidityTest.java
+++ src/e2e/java/teammates/e2e/util/TestDataValidityTest.java
@@ -73,7 +73,7 @@ public class TestDataValidityTest extends BaseTestCase {
                     return;
                 }
 
-                String testPage = path.getFileName().toString().replace("E2ETestSql.json", "");
+                String testPage = extractTestPage(path.getFileName().toString());
                 SqlDataBundle dataBundle = JsonUtils.fromJson(jsonString, SqlDataBundle.class);
 
                 dataBundle.accounts.forEach((id, account) -> {
@@ -196,6 +196,15 @@ public class TestDataValidityTest extends BaseTestCase {
             return true;
         }
         return googleId.matches(constructIdRegex(testPage)) && googleId.length() < 32;
+    }
+
+    private String extractTestPage(String fileName) {
+        return fileName
+                .replaceFirst("\\.json$", "")
+                .replaceFirst("_SqlEntities$", "")
+                .replaceFirst("E2ESqlTest$", "E2ETest")
+                .replaceFirst("Sql$", "")
+                .replaceFirst("E2ETest$", "");
     }
 
     private String constructIdRegex(String testPage) {
